9b99fe2ebb079a1aecf4cad1afaa98daa9c39d1c
PeaceAyomide <133246332+PeaceAyomide@users.noreply.github.com>
2024-01-02 08:03:34 +0100

    Update Vpro.jsx

--- src/components/Vpro/Vpro.jsx
+++ src/components/Vpro/Vpro.jsx
@@ -17,14 +17,14 @@ const Vpro = () => {
   return (
     <section class="about" id="about">
 <div class="about-img">
-   <img  data-aos="fade-up" data-aos-offset="300"
+   <img  data-aos="fade-up" data-aos-once="true" data-aos-offset="300"
         data-aos-easing="ease-in-sine" src={VPRO} alt=""/> 
 </div>
 
 <div class="about-content">
-<h2 data-aos="fade-up"  data-aos-easing="linear"
+<h2 data-aos="fade-up"  data-aos-easing="linear" data-aos-once="true"
      class="heading">New <span>VPRO</span></h2>
-<p data-aos="fade-left" data-aos-duration="20000" >Apple VPRO is a cutting-edge product lineup by Apple Inc. featuring the powerful SuperChip, stunning UltraVision Display, advanced security with SecureGuard, intuitive iSense controls, fast 5G connectivity with SkyWave, high-quality audio through AirPods Max, and health tracking with iFit. It also includes the convenient MagLev magnetic charging system. Apple VPRO showcases Apple's commitment to innovation and quality in the realm of consumer electronics.</p>
+<p data-aos="fade-left" data-aos-duration="20000" data-aos-once="true" >Apple VPRO is a cutting-edge product lineup by Apple Inc. featuring the powerful SuperChip, stunning UltraVision Display, advanced security with SecureGuard, intuitive iSense controls, fast 5G connectivity with SkyWave, high-quality audio through AirPods Max, and health tracking with iFit. It also includes the convenient MagLev magnetic charging system. Apple VPRO showcases Apple's commitment to innovation and quality in the realm of consumer electronics.</p>
 </div>
 </section>
 
